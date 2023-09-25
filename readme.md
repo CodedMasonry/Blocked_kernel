f90812d9a9dc3d58358c81bf09a77be7e691312e
Mason <109301662+NativeCognitive@users.noreply.github.com>
2023-09-25 19:18:53 -0400

    Update to end of chapter 2

---
+++ readme.md
@@ -0,0 +1,5 @@
+"Make an os" they said, "make ~tetris~ block stacking" they said, "it will be fun" they said.
+
+pain.
+
+(recommend reading https://os.phil-opp.com/ to understand what is going on)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // Don't link the std library
+#![no_main] // Disable default entry points
+
+use core::panic::PanicInfo;
+
+// Handle panics because core doesn't have that
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle] // Don't skew the name
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
