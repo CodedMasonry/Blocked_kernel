683736104e737a73a8c4bfd26a686288d7b3bdd5
Mason <109301662+NativeCognitive@users.noreply.github.com>
2023-09-26 16:45:44 -0400

    Add color reference

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std] // Don't link the std library
 #![no_main] // Disable default entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // Handle panics because core doesn't have that
---
+++ src/vga_buffer.rs
@@ -0,0 +1,41 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ColorCode(u8);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct ScreenChar {
+    ascii_char: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_SIZE = 
+
+
+impl ColorCode {
+    pub fn new(foreground: Color, background: Color) -> Self {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
